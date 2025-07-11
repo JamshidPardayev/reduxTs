d7696b3629c7d14be550c56a9e2dabdd0f36e833
JamshidPardayev <jamshidnt2024@gmail.com>
2025-07-11 15:57:58 +0500

    hatolik tugirlandi

--- src/hooks/useTodos.tsx
+++ src/hooks/useTodos.tsx
@@ -1,14 +1,16 @@
-// useTodos.ts
-import type { ITodos } from "../types";
-import { api } from "../api";
+// hooks/useTodos.ts
 import { useQuery } from "@tanstack/react-query";
+import { api } from "../api";
+import type { ITodos } from "../types";
 
 export const useTodos = () => {
   return useQuery<ITodos[]>({
     queryKey: ["todos"],
     queryFn: async () => {
-      const res = await api.get("/todos");
-      return res.data;
+      const { data } = await api.get<ITodos[]>("/todos");
+      return data;
     },
+    staleTime: 1000 * 60 * 5,
+    retry: 1,
   });
 };
--- src/hooks/useUsers.tsx
+++ src/hooks/useUsers.tsx
@@ -1,13 +1,15 @@
-import type { IUsers } from "../types";
 import { api } from "../api";
 import { useQuery } from "@tanstack/react-query";
+import type { IUsers } from "../types";
 
 export const useUsers = () => {
   return useQuery<IUsers[]>({
     queryKey: ["users"],
     queryFn: async () => {
-      const res = await api.get("/users");
-      return res.data;
+      const { data } = await api.get<IUsers[]>("/users");
+      return data;
     },
+    staleTime: 1000 * 60 * 5,
+    retry: 1,
   });
 };
